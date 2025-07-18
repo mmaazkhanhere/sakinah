16875de36d6b5d6fd7f22ec05cb44cb9da57ae28
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-07-18 15:48:56 +0500

    feat: chunking of quran csv file done

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+from colorama import Fore, Style, init
+
+from create_ayah_chunks import create_ayah_chunks
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=f'{Fore.BLUE}%(asctime)s{Style.RESET_ALL} - {Fore.GREEN}%(levelname)s{Style.RESET_ALL} - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+data_path = "data/quran.csv"
+
+chunks = create_ayah_chunks(data_path)
+
+logger.info(f"{Fore.GREEN}First chunk: {chunks[0]}{Style.RESET_ALL}")
+logger.info(f"{Fore.GREEN}Second chunk: {chunks[1]}{Style.RESET_ALL}")
+logger.info(f"{Fore.GREEN}Last chunk: {chunks[-1]}{Style.RESET_ALL}")
